63e72c877d7998b4b8147763ece5b112e116460b
ZYChimne <gazlayxyc@outlook.com>
2021-03-04 21:44:35 +0800

    v1.0

--- app/src/main/java/com/example/android/guesstheword/screens/translate/TranslateViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/translate/TranslateViewModel.kt
@@ -1,10 +1,26 @@
 package com.example.android.guesstheword.screens.translate
 
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.guesstheword.R
 import java.net.URL
 
 class TranslateViewModel: ViewModel() {
+
+    private val _srcPos = MutableLiveData<Long>()
+    val srcPos: LiveData<Long>
+        get() = _srcPos
+
+    private val _tarPos = MutableLiveData<Long>()
+    val tarPos: LiveData<Long>
+        get() = _tarPos
+
+
     init{
+
 
     }
 
