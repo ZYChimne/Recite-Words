45e64564ce8279b78b5e12009549cdc72d01bc4f
ZYChimne <gazlayxyc@outlook.com>
2021-03-04 20:08:03 +0800

    v1.0

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -18,16 +18,61 @@ package com.example.android.guesstheword
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.Collections.list
 
 /**
  * Creates an Activity that hosts all of the fragments in the app
  */
 class MainActivity : AppCompatActivity() {
 
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+//        if (savedInstanceState == null) {
+//            setupBottomNavigationBar()
+//        } // Else, need to wait for onRestoreInstanceState
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        //setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+//    private fun setupBottomNavigationBar() {
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.botto)
+//
+//        val navGraphIds = listOf(R.navigation.home, R.navigation.list, R.navigation.form)
+//
+//        // Setup the bottom navigation view with a list of navigation graphs
+//        val controller = bottomNavigationView.setupWithNavController(
+//                navGraphIds = navGraphIds,
+//                fragmentManager = supportFragmentManager,
+//                containerId = R.id.nav_host_container,
+//                intent = intent
+//        )
+//
+//        // Whenever the selected controller changes, setup the action bar.
+//        controller.observe(this, Observer { navController ->
+//            setupActionBarWithNavController(navController)
+//        })
+//        currentNavController = controller
+//    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 
 }
